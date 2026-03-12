4620102ee00db9e61bfced07ab5bba0ba9598226
ettela <muyan@stu.njtc.edu.cn>
2026-03-12 12:56:25 +0800

    class exercise

--- src/example1_2.py
+++ src/example1_2.py
@@ -1,7 +1,4 @@
-# example1_2.py
-# coding=UTF-8
-
-def with_indent():
+def factorial_sum():
     n = input("输入一个整数：")
     n = int(n)
     k = 1
@@ -14,21 +11,6 @@ def with_indent():
     print("sums=%i" % sum)
 
 
-def no_indent():
-    n = input("输入一个整数：")
-    n = int(n)
-    k = 1
-    sum = 0
-
-    for i in range(1, n + 1):
-        k = k * i
-    sum = sum + k
-
-    print("sums=%i" % sum)
-
-
-
 if __name__ == "__main__":
-    # right()
-    with_indent()
-    no_indent()
+    factorial_sum()
+    # sums=4037913
