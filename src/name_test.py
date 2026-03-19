0564bb2367415603b3855b199b7eecd0ca10439a
ettela <muyan@stu.njtc.edu.cn>
2026-03-19 10:21:48 +0800

    class exercise

---
+++ src/name_test.py
@@ -0,0 +1,7 @@
+# name_test.py
+def print_name():
+    print(f"当前的 __name__ 是： {__name__}")
+
+
+if __name__ == "__main__":
+    print_name()
---
+++ src/name_test_import.py
@@ -0,0 +1,4 @@
+# name_test_import.py
+import name_test
+
+name_test.print_name()
