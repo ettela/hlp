0228368851fd6f28a1d3c99e64da7899dadefe41
ettela <muyan@stu.njtc.edu.cn>
2026-04-16 02:31:40 +0800

    refactor: simplify input validation in input_list function

--- src/home_work_list.py
+++ src/home_work_list.py
@@ -1,25 +1,26 @@
 def input_list(prompt: str, num: int) -> list[int]:
-    error_msg = f"错误：请输入 {num} 个整数。"
-
     while True:
         _list = input(f"{prompt} （输入 {num} 个整数，空格分隔）：").split()
-        if len(_list) != num:
-            print(error_msg)
+        
+        try:
+            res = list(map(int, _list))
+        except ValueError:
+            print(f"错误：请输入 {num} 个整数。")
             continue
 
-        try:
-            return list(map(int, _list))
-        except ValueError:
-            print(error_msg)
+        if len(res) == num:
+            return res
+        print(f"错误：请输入 {num} 个整数。")
 
 
 def main():
     list1 = input_list("输入 list1", 4)
     list2 = input_list("输入 list2", 3)
-    list1.extend(list2)
-    list1.append(90)
-    list1.append(100)
-    list1.sort(reverse=True)
+    # list1.extend(list2)
+    # list1.append(90)
+    # list1.append(100)
+    # list1.sort(reverse=True)
+    list1 = sorted(list1 + list2 + [90, 100], reverse=True)
     print(f"{list1 = }")
 
 
